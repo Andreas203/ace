1784056bfbfe5b2833872317b39cd318ac8e09e3
Andreas Papakonstantinou <andreas@Andreass-MacBook-Pro.local>
2023-02-18 20:13:03 +0000

    fixed client

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,19 +1,13 @@
 import { World } from './World';
-<<<<<<< HEAD
-import { io } from 'socket.io-client';
-=======
 import { io } from 'socket.io-client'
 import { PerspectiveCamera, Vector3 } from 'three';
 import { Player } from './objects/Player';
->>>>>>> 9c54026 (added logic for adding a match with players on new connections)
 
 async function main () {
 	const socket = io(
 		'ws://localhost:3000', { transports: ['websocket'] }
 	);
 
-<<<<<<< HEAD
-=======
 	var camera: PerspectiveCamera = new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 
@@ -21,7 +15,6 @@ async function main () {
 		camera.position.set(p.x, p.y, p.z);
 	})
 
->>>>>>> 9c54026 (added logic for adding a match with players on new connections)
 	const container = document.body;
 
 	const world = new World(container, camera);
@@ -29,9 +22,11 @@ async function main () {
 	await world.init();
 
 	socket.on('playerPosition', (p) => {
+		console.log('newPlayer connected');
 		world.addPlayer(new Player(p));	
 	})
 	
+
 	world.start();
 }
 
