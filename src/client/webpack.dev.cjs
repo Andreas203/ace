b7bea244dd31f16cc26eb9dc22347b9301e2179d
Andreas Papakonstantinou <andreas@Andreass-MacBook-Pro.local>
2023-03-31 12:30:02 +0100

    add new configs

---
+++ src/client/webpack.dev.cjs
@@ -0,0 +1,14 @@
+const { merge } = require('webpack-merge')
+const base = require('./webpack.common.cjs')
+const path = require('path')
+
+module.exports = merge(base, {
+    mode: 'development',
+    devtool: 'eval-source-map',
+    devServer: {
+        static: {
+            directory: path.join(__dirname, '../../dist/client'),
+        },
+        hot: true,
+    },
+})
